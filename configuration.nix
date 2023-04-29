6b27462ec18cbf845118bb15e70b021dcca19e02
OpenSky <82097160+opensky0@users.noreply.github.com>
2023-04-29 03:36:06 +0300

    Add files via upload

--- configuration.nix
+++ configuration.nix
@@ -25,10 +25,6 @@
   # Set your time zone.
   time.timeZone = "Europe/Bucharest";
 
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
-
   # Select internationalisation properties.
   # i18n.defaultLocale = "en_US.UTF-8";
   # console = {
@@ -53,9 +49,6 @@
   #   "eurosign:e";
   #   "caps:escape" # map caps to escape.
   # };
-
-  # Enable CUPS to print documents.
-  # services.printing.enable = true;
 
   # Enable sound.
   sound.enable = true;
